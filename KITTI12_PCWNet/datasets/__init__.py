9037c98a1fc8acffa8b4e78d45148114d5117e78
zhengdian1 <55654518+zhengdian1@users.noreply.github.com>
2024-03-17 14:44:14 +0800

    Delete KITTI12_PCWNet directory

--- KITTI12_PCWNet/datasets/__init__.py
+++
@@ -1,7 +0,0 @@
-from .kitti_dataset import KITTIDataset
-from .sceneflow_dataset import SceneFlowDatset
-
-__datasets__ = {
-    "sceneflow": SceneFlowDatset,
-    "kitti": KITTIDataset
-}
--- KITTI12_PCWNet/scripts/kitti12.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -x
-DATAPATH="/home/zhengdian/dataset/KITTI/2012/"
-CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py --dataset kitti \
-    --datapath $DATAPATH --trainlist ./filenames/kitti12_train.txt --testlist ./filenames/kitti12_val.txt \
-    --epochs 300  --lr 0.001 --batch_size 4 --lrepochs "200:10" \
-    --model pcw_ddim --logdir ./checkpoints/kitti12/test \
-    --test_batch_size 12
